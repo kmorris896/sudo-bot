2be7911b6cbf79d60db501878a55fb68b019f454
Kelly Morris <70837349+kmorris896@users.noreply.github.com>
2020-12-27 12:37:37 -0500

    Show who invoked and which roles they have

--- commands/sudo.js
+++ commands/sudo.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'sudo',
     description: 'sudo!',
     execute(msg, args) {
-      msg.reply("I've been `sudo`'d");
-      // msg.channel.send('bar');
+      // msg.reply("I've been `sudo`'d");
+      msg.channel.send(msg.member + " invoked me and has the following roles: " + JSON.stringify(msg.member.roles));
     },
 };
