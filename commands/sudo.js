2210c14711cfce4ff4445ceededbc1389b5f79a7
Kelly Morris <70837349+kmorris896@users.noreply.github.com>
2020-12-27 16:58:38 -0500

    made server-based config

--- commands/sudo.js
+++ commands/sudo.js
@@ -1,23 +1,45 @@
-const sudoTimeout = 1000 * 60 * 5; // 5 minutes
-// const sudoTimeout = 1000 * 10; // 10 seconds
-const wheelGID = '792816454407553075';
+// const sudoTimeout = 1000 * 60 * 5; // 5 minutes
+const sudoTimeout = 1000 * 10; // 10 seconds
+const sudoersGID = '792816454407553075';
 const suGID = '782265818015858749';
 
+const config = {
+  "704057794571272362": {
+    "sudoersGID": "792816454407553075",
+    "suGID": "782265818015858749"
+  },
+  "752074304224755752": {
+    "sudoersGID": "792816454407553075",
+    "suGID": "782265818015858749"
+  }
+}
+
+
 function removeSudo(msgObj) {
-  msgObj.member.roles.remove(suGID);
-  msgObj.channel.send(msgObj.member + " has lost admin access.");
+  if(config.hasOwnProperty(msg.guild.id)) {
+    const suGID = config[msg.guild.id].suGID;
+
+    msgObj.member.roles.remove(suGID);
+    msgObj.channel.send(msgObj.member + " has lost admin access.");
+  }
 }
 
 module.exports = {
     name: 'sudo',
     description: 'sudo!',
     execute(msg, args) {
-      // msg.reply("I've been `sudo`'d"); 
       const memberDisplayName = msg.member.displayName + " (" + msg.member.id + ")";
-      if(msg.member.roles.cache.has(wheelGID)) {
-        msg.member.roles.add(suGID);
-        setTimeout(removeSudo, sudoTimeout, msg);
-        msg.channel.send(memberDisplayName + " now has admin access for " + (sudoTimeout/(1000*60)) + " minutes.");
+
+      // Look up the configuration for the server
+      if(config.hasOwnProperty(msg.guild.id)) {
+        const sudoersGID = config[msg.guild.id].sudoersGID;
+        const suGID = config[msg.guild.id].suGID;
+
+        if(msg.member.roles.cache.has(sudoersGID)) {
+          msg.member.roles.add(suGID);
+          setTimeout(removeSudo, sudoTimeout, msg);
+          msg.channel.send(memberDisplayName + " now has admin access for " + (sudoTimeout/(1000*60)) + " minutes.");
+        }
       }
     },
 };
