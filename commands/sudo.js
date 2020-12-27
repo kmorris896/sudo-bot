436617a36bd3d5d488fe2e2a86858789876fdfde
Kelly Morris <70837349+kmorris896@users.noreply.github.com>
2020-12-27 13:23:21 -0500

    updates to sudo

--- commands/sudo.js
+++ commands/sudo.js
@@ -3,8 +3,10 @@ module.exports = {
     description: 'sudo!',
     execute(msg, args) {
       // msg.reply("I've been `sudo`'d");
-      if(msg.member.roles.has('762019329771962420')) {
-        msg.channel.send(msg.member + " invoked me and has the admin role.");
+      if(msg.member.roles.has('792816454407553075')) {
+        msg.channel.send(msg.member + " invoked me and has the wheel role.");
+        msg.member.roles.add('782265818015858749');
+        msg.channel.send(msg.member + " now has admin access for 10 minutes.")
       }      
     },
 };
