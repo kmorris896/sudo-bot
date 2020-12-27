da4dc35752e39fba67ff56c540c8c9a6172bfabc
Kelly Morris <70837349+kmorris896@users.noreply.github.com>
2020-12-27 12:41:36 -0500

    changed to cache

--- commands/sudo.js
+++ commands/sudo.js
@@ -3,6 +3,6 @@ module.exports = {
     description: 'sudo!',
     execute(msg, args) {
       // msg.reply("I've been `sudo`'d");
-      msg.channel.send(msg.member + " invoked me and has the following roles: " + JSON.stringify(msg.member.roles));
+      msg.channel.send(msg.member + " invoked me and has the following roles: " + JSON.stringify(msg.member.roles.cache));
     },
 };
